a7dbbdd7f58b0eb50046636f11d4fc68bf810539
Evgeniy <vrd096@mail.ru>
2020-03-06 15:11:39 +0500

    событие при клике бургера, смена класса

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -3,10 +3,24 @@
 (function() {
   let handleBurger = document.querySelector(".nav__toggle");
   let navMenu = document.querySelector(".nav__list");
-
+  const cls = {
+    open: "nav__toggle-burger--opened",
+    close: "nav__toggle-burger--closed"
+  };
+  let btnClass = cls.close;
 
   handleBurger.addEventListener("click", function() {
-    navMenu.classList.toggle("nav__menu--opended");
-    handleBurger.classList.toggle("nav__toggle-burger--opened");
+    navMenu.classList.toggle("nav__menu--opened");
+
+    if (handleBurger.classList.contains(cls.close)) {
+      handleBurger.classList.remove(btnClass);
+      handleBurger.classList.add(cls.open);
+      btnClass = cls.open;
+    }
+    else if (handleBurger.classList.contains(cls.open)) {
+      handleBurger.classList.remove(btnClass);
+      handleBurger.classList.add(cls.close);
+      btnClass = cls.close;
+    }
   });
 })();
