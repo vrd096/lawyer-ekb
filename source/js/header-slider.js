3ad020e9fb462d3f91d4842979dfa0b829b77646
vrd096 <48715486+vrd096@users.noreply.github.com>
2020-03-16 21:56:28 +0500

    корректировки css, разработка слайдера

--- source/js/header-slider.js
+++ source/js/header-slider.js
@@ -5,24 +5,41 @@
     let sliderImgTwo = document.querySelector(".js-slider-two");
     let sliderTitleOne = document.querySelector(".js-slider-title-one");
     let sliderTitleTwo = document.querySelector(".js-slider-title-two");
+    let sliderLineOne = document.querySelector(".js-slider-line-one");
+    let sliderLineTwo = document.querySelector(".js-slider-line-two");
+    let sliderSubtitleOne = document.querySelector(".js-slider-subtitle-one");
+    let sliderSubtitleTwo = document.querySelector(".js-slider-subtitle-two");
     let wrapperOne = document.querySelector(".main-header__slider-block-one");
     let wrapperTwo = document.querySelector(".main-header__slider-block-two");
+    let NumberPages = document.querySelector(".main-header__slider-pages");
+
+    function nextPage(item) {
+      if (item.includes("01/02")) {
+        item.textContent = "02/02";
+      }
+      if (item.includes("02/02")) {
+        item.textContent = "01/02";
+      }
+
+    }
 
     function nextBacgroundImage() {
       sliderImgTwo.classList.toggle("main-header__slider-show");
     }
     function nextTitle() {
-
       sliderTitleOne.classList.toggle("main-header__slider-title--show");
 
       sliderTitleTwo.classList.toggle("main-header__slider-title--show");
     }
-    function hideBlock () {
-      wrapperOne.classList.toggle("main-header__slider-block--show");
+    function changeBlock(item) {
+      item.classList.toggle("main-header__slider-block--show");
     }
-    function showBlock () {
-      wrapperTwo.classList.toggle("main-header__slider-block--show");
+    function transformText(item) {
+      item.classList.toggle("main-header__slider--transform");
     }
+    // function showBlock(item) {
+    //   item.classList.add("main-header__slider-block--show");
+    // }
 
     const sliderTimer = time => {
       return new Promise((resolve, reject) => setTimeout(resolve, time));
@@ -31,18 +48,26 @@
     window.setInterval(function() {
       sliderTimer(3000)
         .then(() => {
+          nextPage(NumberPages);
           nextBacgroundImage();
-          hideBlock();
-          return sliderTimer(2000);
-        })
-        .then(() => {
-          showBlock ()
-          // nextTitle();
+          changeBlock(wrapperOne);
+          changeBlock(wrapperTwo);
           return sliderTimer(1000);
         })
         .then(() => {
-          console.log("3й шаг");
+          transformText(sliderTitleOne);
+          transformText(sliderTitleTwo);
+          return sliderTimer(1000);
+        })
+        .then(() => {
+          transformText(sliderLineOne);
+          transformText(sliderLineTwo);
+          return sliderTimer(100);
+        })
+        .then(() => {
+          transformText(sliderSubtitleOne);
+          transformText(sliderSubtitleTwo);
         });
-    }, 5000);
+    }, 10000);
   });
 })();
