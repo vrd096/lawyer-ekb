312052eec4e15788a7b9e7e0ca6e11101d41ad38
vrd096 <48715486+vrd096@users.noreply.github.com>
2020-03-19 22:57:11 +0500

    при смене местами файлы js кто то выбивается из работы

--- source/js/scroll-menu.js
+++ source/js/scroll-menu.js
@@ -9,12 +9,11 @@
     if (!scroll && window.pageYOffset > 50) {
       scroll = true;
       nav.classList.add("nav-is-scroll");
-      // console.log("show");
-      // headerSlider.classList.add("main-header__slider-container--margin");
+      headerSlider.classList.add("main-header__slider-container--margin");
     } else if (scroll && window.pageYOffset <= 50) {
       scroll = false;
       nav.classList.remove("nav-is-scroll");
-      // headerSlider.classList.remove("main-header__slider-container--margin");
+      headerSlider.classList.remove("main-header__slider-container--margin");
     }
   }
 
