1204e07f9432d6d2e5866627af3458d713c3e0fd
vrd096 <48715486+vrd096@users.noreply.github.com>
2020-09-18 15:42:08 +0500

    настроил отображение булетов в слайдере

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,10 +1,19 @@
 "use strict";
 
-let slider = new Glide('.glide', {
-	type: 'carousel',
-	startAt: 0,
-	perView: 2,
-	autoplay: 3000
-  });
+let slider = new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 2,
+  autoplay: 3000,
+  breakpoints: {
+    1024: {
+      perView: 2,
+    },
+    768: {
+      perView: 1,
+    },
+  },
+  
+});
 
 slider.mount();
