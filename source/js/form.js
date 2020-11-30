5d3ae6a7b1321f018a0f601b7221a0c340c1d4b5
vrd096 <48715486+vrd096@users.noreply.github.com>
2020-11-30 19:41:11 +0500

    удалил лишний код

--- source/js/form.js
+++ source/js/form.js
@@ -1,14 +1,12 @@
 "use strict";
 
-(function() {
-  let modelSend = document.querySelector(".modal-send");
+(function () {
   let formUpload = document.querySelector(".footer__feedback-form");
   let formConsul = document.querySelector(".modal-send__form");
   let popupError = document.querySelector(".popup-error");
   let popupErrorText = document.querySelector(".popup-error__text");
   let ESC_KEYCODE = 27;
 
- 
   function resetForm() {
     formUpload.reset();
     formConsul.reset();
@@ -38,16 +36,11 @@
     document.addEventListener("keyup", handleEsq);
   }
 
-
-  // function translate(text) {
-  //   return translations[text] || text;
-  // }
-
   function errorHandler(error) {
     popupErrorText.textContent = error.message;
 
     if (error.status === 400) {
-      error.response.forEach(function(field) {
+      error.response.forEach(function (field) {
         let fieldEl = document.getElementById(field.fieldName);
         fieldEl.style = "border: 1px solid red";
       });
@@ -64,9 +57,6 @@
 
     openError();
   }
-  // function closeConsul() {
-  //   modelSend.classList.add("visually-hidden");
-  // }
 
   formUpload.addEventListener("submit", function onFormSubmit(evt) {
     window.saveForm(new FormData(formUpload), resetForm, errorHandler);
