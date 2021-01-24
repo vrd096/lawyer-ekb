b4b1c13f0bb11c4152b5b74a516b8de974029660
vrd096 <48715486+vrd096@users.noreply.github.com>
2021-01-24 12:08:23 +0500

    запрограммировал кнопку заказать, стилизовал аккардион

--- source/js/modal-form.js
+++ source/js/modal-form.js
@@ -1,18 +1,18 @@
 "use strict";
 
-(function() {
+(function () {
   let modelSend = document.querySelector(".modal-send");
   let modelOverlay = document.querySelector(".modal-overlay");
-  let contactButton = document.querySelector(".nav__consultation");
+  let contactButtons = document.querySelectorAll(".js-consultation");
   let modalClose = document.querySelector(".modal-send__close");
   const ESC_KEYCODE = 27;
 
-  window.closeModal = function closeModal (){
+  window.closeModal = function closeModal() {
     modelSend.classList.add("visually-hidden");
     modelOverlay.classList.add("visually-hidden");
 
     document.removeEventListener("keyup", handleCardEsc);
-  }
+  };
 
   function showModal() {
     modelSend.classList.remove("visually-hidden");
@@ -26,7 +26,8 @@
       closeModal();
     }
   }
-  
-  contactButton.addEventListener("click", showModal);
+  console.log(contactButtons);
+  contactButtons.forEach((item) => item.addEventListener("click", showModal));
+
   modalClose.addEventListener("click", closeModal);
 })();
--- source/js/price.js
+++ source/js/price.js
@@ -4,14 +4,15 @@
   let elements = document.querySelectorAll(".price__item");
 
   elements.forEach((item) =>
-    item.addEventListener("click", function () {
-      if (this.classList.contains("price__item-active")) {
-        this.classList.remove("price__item-active");
+    item.childNodes[1].addEventListener("click", function () {
+      
+      if (item.classList.contains("price__item-active")) {
+        item.classList.remove("price__item-active");
       } else {
         elements.forEach((child) =>
           child.classList.remove("price__item-active")
         );
-        this.classList.add("price__item-active");
+        item.classList.add("price__item-active");
       }
     })
   );
