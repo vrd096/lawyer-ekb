9575dc62c36ab1928abe6e2a5b8e302602b790d1
vrd096 <48715486+vrd096@users.noreply.github.com>
2021-01-25 20:09:02 +0500

    заполнил хиро

--- source/js/scroll-menu.js
+++ source/js/scroll-menu.js
@@ -6,17 +6,20 @@
     let headerSlider = document.querySelector(
       ".header__slider-container"
     );
+    let headerHero = document.querySelector(
+      ".header__slider-container"
+    );
     let scroll = false;
 
     function toggleScroll() {
       if (!scroll && window.pageYOffset > 50) {
         scroll = true;
         nav.classList.add("nav-is-scroll");
-        // headerSlider.classList.add("header__slider-container--margin");
+        headerSlider.classList.add("header__slider-container--margin");
       } else if (scroll && window.pageYOffset <= 50) {
         scroll = false;
         nav.classList.remove("nav-is-scroll");
-        // headerSlider.classList.remove("header__slider-container--margin");
+        headerSlider.classList.remove("header__slider-container--margin");
       }
     }
 
